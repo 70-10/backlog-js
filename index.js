1693d50412290402382fc9f29ae082213f3a6a45
70_10 <7010.dev@gmail.com>
2017-10-29 23:26:19 +0900

    [mod] change baseURL

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const qs = require("querystring");
 class Backlog {
   constructor(options = {}) {
     this.client = axios.create({
-      baseURL: `https://${options.spaceKey || ""}.backlog.jp`,
+      baseURL: `https://${options.spaceKey || ""}.backlog.jp/api/v2`,
       params: { apiKey: options.apiKey }
     });
   }
 
   projects(all = false, archived = false) {
     return this.client
-      .get("/api/v2/projects", { params: { all, archived } })
+      .get("/projects", { params: { all, archived } })
       .then(res => res.data);
   }
   projectUsers(id) {
-    return this.client
-      .get(`/api/v2/projects/${id}/users`)
-      .then(res => res.data);
+    return this.client.get(`/projects/${id}/users`).then(res => res.data);
   }
 }
 
